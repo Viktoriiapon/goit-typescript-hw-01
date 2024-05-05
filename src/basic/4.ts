55788ec67e8be70831c59487349a0dc7ba1cabf6
Viktoriiapon <viktoriyaponom@gmail.com>
2024-05-05 21:22:32 +0200

    tasks ready

---
+++ src/basic/4.ts
@@ -0,0 +1,12 @@
+
+function showMessage(message):void {
+    console.log(message);
+  }
+  
+  function calc(num1, num2):number {
+    return num1 + num2;
+  }
+  
+  function customError():never {
+    throw new Error('Error');
+  }
---
+++ src/generics/4.ts
@@ -0,0 +1,15 @@
+
+interface User {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+  }
+  
+  function createOrUpdateUser(initialValues: Partial<User>) {
+    return {...initialValues };
+  }
+  
+ 
+
+  
